84d46daf6ee84dbca16e432d04687f5a62d8de52
julius caezar <julius.caezar25@gmail.com>
2026-02-11 01:17:22 -0300

    feat: create deploy section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import { Deploy } from "@/components/home/deploy";
 import { Hero } from "@/components/home/hero";
 
 export default function Home() {
   return (
     <div>
       <Hero />
+      <Deploy />
     </div>
   );
 }
